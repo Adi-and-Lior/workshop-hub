ffa4ad6ebcba2afec2c84bececaa6656cd679a40
LiorBoaron <lior.boaron8@gmail.com>
2026-01-08 21:09:58 +0200

    feat: wrap App with ThemeProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'; // ייבוא ה-Provider של Redux
+import { store } from './store/store'; // ייבוא ה-Store שיצרנו
 import App from './App.jsx'
 import 'bulma/css/bulma.min.css';
 import './App.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
